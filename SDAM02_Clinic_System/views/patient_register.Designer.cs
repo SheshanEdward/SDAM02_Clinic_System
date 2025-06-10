e21af89902be2e59c716f06b640675183a4e4d24
bluemoon-24 <sarahmathloob.2469@gmail.com>
2025-06-10 23:53:15 +0530

    Patient registration dashboard/UI

---
+++ SDAM02_Clinic_System/views/patient_register.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SDAM02_Clinic_System.views
+{
+    partial class patient_register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            FirstNamelbl = new Label();
+            doblbl = new Label();
+            Heightlbl = new Label();
+            Contactlbl = new Label();
+            Emaillbl = new Label();
+            registerPatientlbl = new Label();
+            LastNamelbl = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            NIClbl = new Label();
+            Weightlbl = new Label();
+            BloodGrouplbl = new Label();
+            mhlbl = new Label();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            Registerbtn = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(114, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(239, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(121, 129);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(232, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(120, 191);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(232, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(121, 255);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(232, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(495, 251);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(239, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // FirstNamelbl
+            // 
+            FirstNamelbl.AutoSize = true;
+            FirstNamelbl.Location = new Point(16, 74);
+            FirstNamelbl.Name = "FirstNamelbl";
+            FirstNamelbl.Size = new Size(84, 20);
+            FirstNamelbl.TabIndex = 5;
+            FirstNamelbl.Text = "First Name ";
+            FirstNamelbl.Click += label1_Click;
+            // 
+            // doblbl
+            // 
+            doblbl.AutoSize = true;
+            doblbl.Location = new Point(17, 132);
+            doblbl.Name = "doblbl";
+            doblbl.Size = new Size(40, 20);
+            doblbl.TabIndex = 6;
+            doblbl.Text = "DOB";
+            // 
+            // Heightlbl
+            // 
+            Heightlbl.AutoSize = true;
+            Heightlbl.Location = new Point(17, 196);
+            Heightlbl.Name = "Heightlbl";
+            Heightlbl.Size = new Size(58, 20);
+            Heightlbl.TabIndex = 7;
+            Heightlbl.Text = "Height ";
+            // 
+            // Contactlbl
+            // 
+            Contactlbl.AutoSize = true;
+            Contactlbl.Location = new Point(15, 256);
+            Contactlbl.Name = "Contactlbl";
+            Contactlbl.Size = new Size(64, 20);
+            Contactlbl.TabIndex = 8;
+            Contactlbl.Text = "Contact ";
+            // 
+            // Emaillbl
+            // 
+            Emaillbl.AutoSize = true;
+            Emaillbl.Location = new Point(375, 258);
+            Emaillbl.Name = "Emaillbl";
+            Emaillbl.Size = new Size(50, 20);
+            Emaillbl.TabIndex = 9;
+            Emaillbl.Text = "Email ";
+            // 
+            // registerPatientlbl
+            // 
+            registerPatientlbl.AutoSize = true;
+            registerPatientlbl.Font = new Font("Segoe UI", 14F);
+            registerPatientlbl.Location = new Point(297, 9);
+            registerPatientlbl.Name = "registerPatientlbl";
+            registerPatientlbl.Size = new Size(227, 32);
+            registerPatientlbl.TabIndex = 10;
+            registerPatientlbl.Text = "Patient Registration ";
+            registerPatientlbl.Click += registerPatientlbl_Click;
+            // 
+            // LastNamelbl
+            // 
+            LastNamelbl.AutoSize = true;
+            LastNamelbl.Location = new Point(373, 72);
+            LastNamelbl.Name = "LastNamelbl";
+            LastNamelbl.Size = new Size(83, 20);
+            LastNamelbl.TabIndex = 11;
+            LastNamelbl.Text = "Last Name ";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(484, 68);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(248, 27);
+            textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(489, 127);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(243, 27);
+            textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(493, 191);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(239, 27);
+            textBox8.TabIndex = 14;
+            // 
+            // NIClbl
+            // 
+            NIClbl.AutoSize = true;
+            NIClbl.Location = new Point(383, 130);
+            NIClbl.Name = "NIClbl";
+            NIClbl.Size = new Size(33, 20);
+            NIClbl.TabIndex = 15;
+            NIClbl.Text = "NIC";
+            // 
+            // Weightlbl
+            // 
+            Weightlbl.AutoSize = true;
+            Weightlbl.Location = new Point(374, 194);
+            Weightlbl.Name = "Weightlbl";
+            Weightlbl.Size = new Size(60, 20);
+            Weightlbl.TabIndex = 16;
+            Weightlbl.Text = "Weight ";
+            // 
+            // BloodGrouplbl
+            // 
+            BloodGrouplbl.AutoSize = true;
+            BloodGrouplbl.Location = new Point(13, 309);
+            BloodGrouplbl.Name = "BloodGrouplbl";
+            BloodGrouplbl.Size = new Size(98, 20);
+            BloodGrouplbl.TabIndex = 17;
+            BloodGrouplbl.Text = "Blood Group ";
+            // 
+            // mhlbl
+            // 
+            mhlbl.AutoSize = true;
+            mhlbl.Location = new Point(371, 312);
+            mhlbl.Name = "mhlbl";
+            mhlbl.Size = new Size(117, 20);
+            mhlbl.TabIndex = 18;
+            mhlbl.Text = "Medical History ";
+            mhlbl.Click += mhlbl_Click;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(121, 306);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(231, 27);
+            textBox9.TabIndex = 19;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(492, 306);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(240, 27);
+            textBox10.TabIndex = 20;
+            // 
+            // Registerbtn
+            // 
+            Registerbtn.BackColor = Color.SkyBlue;
+            Registerbtn.Font = new Font("Segoe UI", 12F);
+            Registerbtn.Location = new Point(352, 379);
+            Registerbtn.Name = "Registerbtn";
+            Registerbtn.Size = new Size(110, 43);
+            Registerbtn.TabIndex = 21;
+            Registerbtn.Text = "Register ";
+            Registerbtn.UseVisualStyleBackColor = false;
+            // 
+            // patient_register
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Registerbtn);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(mhlbl);
+            Controls.Add(BloodGrouplbl);
+            Controls.Add(Weightlbl);
+            Controls.Add(NIClbl);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(LastNamelbl);
+            Controls.Add(registerPatientlbl);
+            Controls.Add(Emaillbl);
+            Controls.Add(Contactlbl);
+            Controls.Add(Heightlbl);
+            Controls.Add(doblbl);
+            Controls.Add(FirstNamelbl);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "patient_register";
+            Text = "patient_register";
+            Load += patient_register_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label FirstNamelbl;
+        private Label doblbl;
+        private Label Heightlbl;
+        private Label Contactlbl;
+        private Label Emaillbl;
+        private Label registerPatientlbl;
+        private Label LastNamelbl;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label NIClbl;
+        private Label Weightlbl;
+        private Label BloodGrouplbl;
+        private Label mhlbl;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private Button Registerbtn;
+    }
+}
---
+++ SDAM02_Clinic_System/views/patient_register.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDAM02_Clinic_System.views
+{
+    public partial class patient_register : Form
+    {
+        public patient_register()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registerPatientlbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void patient_register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mhlbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

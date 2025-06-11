2f6fdacf4190b1d5b3f5d9b13d92fd6172316010
Sheshan Edward <sheshanedward2004@gmail.com>
2025-06-11 13:45:57 +0530

    Patient class code improvements

---
+++ SDAM02_Clinic_System/models/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM02_Clinic_System.models
+{
+    internal class Patient : Person
+    {
+        private string patient_id;
+        private string gender;
+        private string bloodtype;
+        private double height;
+        private double weight;
+
+
+        public Patient(string patient_id, string gender, string bloodtype, double height, double weight) : base(NIC, firstname, lastname, DoB, address)
+        {
+            this.patient_id = patient_id;
+            this.gender = gender;
+            this.bloodtype = bloodtype;
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+}
--- SDAM02_Clinic_System/models/Person.cs
+++ SDAM02_Clinic_System/models/Person.cs
@@ -9,7 +9,7 @@ namespace SDAM02_Clinic_System.models
     internal class Person
     {
         private int NIC;
-        private string fristname;
+        private string firstname;
         private string lastname;
         private DateTime DoB;
         private string address;
@@ -17,12 +17,43 @@ namespace SDAM02_Clinic_System.models
         public Person(int NIC, string firstname, string lastname, DateTime DoB, string address)
         {
             this.NIC = NIC;
-            this.fristname = firstname;
+            this.firstname = firstname;
             this.lastname = lastname;
             this.DoB = DoB;
             this.address = address;
         }
 
+        public int nic
+        {
+            get { return NIC; }
+            set { NIC = value; }
+        }
 
+
+
+        public string Firstname
+        {
+            get { return firstname;}
+            set { firstname = value; }
+        }
+
+
+        public string Lastname
+        {
+            get { return lastname;}
+            set { lastname = value;}
+        }
+
+        public DateTime dob
+        {
+            get { return DoB; }
+            set {  DoB = value; }
+        }
+
+        public string Address
+        {
+            get { return address;}
+            set { address = value; }
+        }
     }
 }

797090010d2bc8e079ed33e29a2cd0b64f78e48b
bluemoon-24 <sarahmathloob.2469@gmail.com>
2025-06-12 00:18:24 +0530

    Created admin_dashboard to view all functions accessible to admin

---
+++ SDAM02_Clinic_System/views/admin_dashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDAM02_Clinic_System.views
+{
+    public partial class admin_dashboard : Form
+    {
+        public admin_dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void adminProfilebtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doctorsbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

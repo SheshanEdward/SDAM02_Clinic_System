7227f6410d63880f96371fc12b200e0375bd36e3
bluemoon-24 <sarahmathloob.2469@gmail.com>
2025-06-13 21:36:38 +0530

    Added logic to PatientAllAppointments

--- SDAM02_Clinic_System/views/PatientAllAppointments.Designer.cs
+++ SDAM02_Clinic_System/views/PatientAllAppointments.Designer.cs
@@ -30,36 +30,60 @@
         {
             dgvPatientappointments = new DataGridView();
             btnSchedule = new Button();
+            btnLoad = new Button();
+            btnBack = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvPatientappointments).BeginInit();
             SuspendLayout();
             // 
             // dgvPatientappointments
             // 
             dgvPatientappointments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvPatientappointments.Location = new Point(22, 12);
+            dgvPatientappointments.Location = new Point(44, 12);
             dgvPatientappointments.Name = "dgvPatientappointments";
             dgvPatientappointments.RowHeadersWidth = 51;
-            dgvPatientappointments.Size = new Size(1035, 364);
+            dgvPatientappointments.Size = new Size(776, 364);
             dgvPatientappointments.TabIndex = 0;
+            dgvPatientappointments.CellContentClick += dgvPatientappointments_CellContentClick;
             // 
             // btnSchedule
             // 
-            btnSchedule.Location = new Point(480, 388);
+            btnSchedule.Location = new Point(315, 392);
             btnSchedule.Name = "btnSchedule";
-            btnSchedule.Size = new Size(121, 50);
+            btnSchedule.Size = new Size(212, 36);
             btnSchedule.TabIndex = 1;
             btnSchedule.Text = "Schedule New Appointment";
             btnSchedule.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(45, 399);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(96, 29);
+            btnLoad.TabIndex = 2;
+            btnLoad.Text = "Load ";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(721, 394);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(94, 29);
+            btnBack.TabIndex = 3;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
             // 
             // PatientAllAppointments
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1084, 450);
+            ClientSize = new Size(883, 450);
+            Controls.Add(btnBack);
+            Controls.Add(btnLoad);
             Controls.Add(btnSchedule);
             Controls.Add(dgvPatientappointments);
             Name = "PatientAllAppointments";
             Text = "PatientAllAppointments";
+            Load += PatientAllAppointments_Load;
             ((System.ComponentModel.ISupportInitialize)dgvPatientappointments).EndInit();
             ResumeLayout(false);
         }
@@ -68,5 +92,7 @@
 
         private DataGridView dgvPatientappointments;
         private Button btnSchedule;
+        private Button btnLoad;
+        private Button btnBack;
     }
 }
--- SDAM02_Clinic_System/views/PatientAllAppointments.cs
+++ SDAM02_Clinic_System/views/PatientAllAppointments.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,82 @@ namespace SDAM02_Clinic_System.views
 {
     public partial class PatientAllAppointments : Form
     {
-        public PatientAllAppointments()
+        private string patientId;
+        private object dvgPatientappointments;
+
+        public PatientAllAppointments(string patientId)
         {
             InitializeComponent();
+            this.patientId = patientId;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadAppointments();
+        }
+
+        private void btnSchedule_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new PatientScheduleAppointment(patientId).Show();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new PatientDashboard(patientId).Show();
+        }
+
+        private void LoadAppointments()
+        {
+            string connectionString = "server=localhost;user=root;password=;database=clinic_system_db;";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = @"
+                    SELECT 
+                        a.appointment_id AS 'Appointment ID',
+                        CONCAT(d.firstname, ' ', d.lastname) AS 'Doctor Name',
+                        s.service_name AS 'Service',
+                        a.appointment_date AS 'Date & Time',
+                        a.status AS 'Status',
+                        a.notes AS 'Notes'
+                    FROM appointment_data a
+                    JOIN doctor_profiles d ON a.doctor_id = d.doctor_id
+                    JOIN clinic_service s ON a.service_id = s.service_id
+                    WHERE a.patient_id = @patientId
+                    ORDER BY a.appointment_date DESC;
+                ";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@patientId", patientId);
+
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dgvPatientappointments.DataSource = dt;
+                    dgvPatientappointments.ReadOnly = true;
+                    dgvPatientappointments.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed to load appointments: " + ex.Message, "Database Error");
+                }
+            }
+
+        }
+        private void dgvPatientappointments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PatientAllAppointments_Load(object sender, EventArgs e)
+        {
+            //Note to myself, please dont delete 
         }
     }
 }

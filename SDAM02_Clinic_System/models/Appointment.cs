ec38f444913e9caff7c91048339ffe140ae7ce5d
Sara Mathloob <sarahmathloob.2469@gmail.com>
2025-06-15 00:47:24 +0530

    Error fixes

--- SDAM02_Clinic_System/models/Appointment.cs
+++ SDAM02_Clinic_System/models/Appointment.cs
@@ -8,26 +8,6 @@ namespace SDAM02_Clinic_System.models
 {
     internal class Appointment
     {
-        public string AppointmentId { get; set; }
-        public string PatientId { get; set; }
-        public string DoctorId { get; set; }
-        public int ServiceId { get; set; }
-        public DateTime AppointmentDate { get; set; }
 
-        // Status: Scheduled, Cancelled
-        public string Status { get; set; }
-
-        public static class AppointmentStatuses
-        {
-            public const string Scheduled = "Scheduled";
-            public const string Cancelled = "Cancelled";
-            public const string Completed = "Completed";
-            public const string NoShow = "No Show";
-        }
-
-        public override string ToString()
-        {
-            return $"[ID: {AppointmentId}] {AppointmentDate:dd MMM yyyy hh:mm tt} - Patient: {PatientId}, Doctor: {DoctorId}, Status: {Status}";
-        }
     }
 }
